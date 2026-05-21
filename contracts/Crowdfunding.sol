8ce971b538ba45c039184548f63ac2addfd255f4
Nancia Rajerison <rajerisonnancia@gmail.com>
2026-05-21 22:31:33 +0300

    Write events that allow the frontend to listen for changes in real time

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -14,5 +14,10 @@ contract Crowdfunding {
     uint256 public campaignCount;
     mapping(uint256 => Campaign) public campaigns;
     mapping(uint256 => mapping(address => uint256)) public contributions;
+
+    event CampaignCreated(uint256 indexed id, address indexed creator, uint256 goal, uint256 deadline);
+    event ContributionReceived(uint256 indexed id, address indexed contributor, uint256 amount);
+    event FundsWithdrawn(uint256 indexed id, uint256 amount);
+    event RefundIssued(uint256 indexed id, address indexed contributor, uint256 amount);
 }
 
